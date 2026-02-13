3d0861c5025d903f1071ee303f7151033f7cfe6a
shivaram0005 <thallojushiva@gmail.com>
2026-02-13 18:06:54 +0530

    added server.js new code

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 
-app.use(
-  cors({
-    origin: "*",
-  }),
-);
-
 const authRoutes = require("./routes/auth");
 const examRoutes = require("./routes/exam");
 const adminRoutes = require("./routes/admin");
@@ -14,11 +8,19 @@ const studentRoutes = require("./routes/student");
 
 const app = express();
 
+// Middlewares
+app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/exam", examRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/student", studentRoutes);
 
-app.listen(5000, () => console.log("Backend running on http://localhost:5000"));
+// Render PORT Fix
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Backend running on port ${PORT}`);
+});
